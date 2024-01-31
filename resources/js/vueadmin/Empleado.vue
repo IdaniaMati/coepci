f685c80f46bd40b5221edffdd8c6d0dc0e6ff945
eac1381 <araos1381@gmail.com>
2024-01-31 15:41:42 -0500

    Mejora de inicio de sesión publico

--- resources/js/vueadmin/Empleado.vue
+++ resources/js/vueadmin/Empleado.vue
@@ -1,17 +1,81 @@
 <template>
     <div class="container">
+      <div class="center-container">
+        <div class="image-container center">
+          <img src="assets/img/logo-2.png" alt="" height="250px" width="450px">
+        </div>
+        <br>
+        <div class="sign-title">
+          <h1 class="title-text">Sistema de Votación para el Comité de Ética y Prevención de Conflicto de Interés</h1>
+          <div class="sign-note">
+            <p class="note-text">Para iniciar la votación es necesario ingresar su CURP</p>
+          </div>
+        </div>
+      </div>
+
       <form>
         <div class="mb-3">
-          <label class="form-label">CURP</label>
+          <label class="note-text">CURP</label>
           <input v-model="curp" type="text" class="form-control" required />
           <div v-if="!curp" class="text-danger">Este campo es obligatorio.</div>
         </div>
 
         <button type="button" class="btn btn-inline btn-primary" @click="ingresar">Login</button>
-        
       </form>
+
+      <div class="announcement-box">
+        <p class="announcement-text">El sistema de votación se encuentra desactivado. <br>Favor de verificar la fecha en la
+            cual el concurso inicie. Si ya se encuentra activo la convocatoria, favor de recargar la página.
+        </p>
+      </div>
+
     </div>
   </template>
+
+  <style scoped>
+  .container {
+    /* display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh; */
+  }
+
+  .center-container {
+    text-align: center;
+  }
+
+  .title-text {
+    font-size: 24px;
+  }
+
+  .note-text {
+    font-size: 16px;
+  }
+
+  .image-container {
+
+  }
+
+  .sign-title {
+
+  }
+
+  .sign-note {
+
+  }
+
+  .announcement-box {
+  background-color: #f0f0f0;
+  padding: 10px;
+  margin-top: 20px;
+}
+
+.announcement-text {
+  color: #555;
+  font-size: 14px;
+  text-align: center;
+}
+  </style>
 
 <script>
   import axios from "axios";
