bdb11e2a4bd2d67b0470d6d0ae23e25c762291a1
IdaniaMati <97311122+IdaniaMati@users.noreply.github.com>
2024-04-03 21:46:49 -0500

    vista y botón exportar RBD (Respaldo Base de Datos)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -30,7 +30,16 @@ class DashboardController extends Controller
         // Obtener todas las tablas de la base de datos "coepci"
         $tables = DB::select('SHOW TABLES FROM coepci');
     
-        $sql = '';
+        $sql = "
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+";
     
         foreach ($tables as $table) {
             $tableName = reset($table);
@@ -63,6 +72,14 @@ class DashboardController extends Controller
                 $sql .= "$values;\n\n";
             }
         }
+
+        $sql.="
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+";
     
         // Descargar el archivo SQL
         $fileName = 'coepci_' . date('Y-m-d_H-i-s') . '.sql';
